08e73a1526b3590ba0317fc83597ce515f0d7de8
unknown <wirayabovorn606@gmail.com>
2026-04-28 11:40:17 +0700

    component

---
+++ src/lib/component/Coin.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from "svelte";
+
+    let {coins = $bindable(0)} = $props();
+    let coin = 0;
+    onMount(()=>{
+        coin = JSON.parse(localStorage.getItem("coins")) || 0;
+    })
+</script>
+
+<p>Coin: {0}</p>
+
+
