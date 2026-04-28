1158a1880fcdd5de00855cbc64f12b142a93919c
unknown <wirayabovorn606@gmail.com>
2026-04-28 13:01:23 +0700

    Update

---
+++ src/routes/games/lightshoot/+page.svelte
@@ -0,0 +1,16 @@
+
+
+<script>
+    let light_color = ["#fcba03", "#009124", "#f70000"]
+    let curr_light = $state(0)
+</script>
+
+
+
+<h1 class="text-center font-bold text-4xl">Click ปุ่มตามสีที่ปรากฏ</h1>
+
+<div class="w-[400px] h-[400px]" style="background-color: {light_color[curr_light]};">
+</div>
+
+
+<button class="bg-black px-5 py-2 cursor-pointer text-white rounded-xl">เริ่มเลย!</button>
